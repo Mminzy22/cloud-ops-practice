43c1be39d3b366e61f43b8f95555bd57cc7cf1ab
Mminzy22 <aw7022@gmail.com>
2025-07-11 17:08:15 +0900

    ✨ feat: Bird, Flyable, Walkable 인터페이스 및 클래스 추가

---
+++ project01/src/main/java/org/example/Bird.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.top.Flyable;
+
+import java.util.Scanner;
+
+public class Bird implements Flyable, Walkable{
+    @Override
+    public void fly() {
+        System.out.println("새가 날아갑니다.");
+    }
+
+    public static void main(String[] args) {
+        Bird myBird = new Bird();
+        myBird.fly();
+        myBird.walk();
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("새가 걷습니다.");
+    }
+}
